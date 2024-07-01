f609237458923d6cc6c1eebc4b25f9316ef1e4b9
Vijesh120503 <94106218+Vijesh120503@users.noreply.github.com>
2024-07-01 22:10:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-var ans = document.getElementById("ans");
-var one = document.getElementById("one");
-var two = document.getElementById("two");
-var three = document.getElementById("three");
-var four = document.getElementById("four");
-var five = document.getElementById("five");
-var next = document.getElementById("next");
+var submitButton=document.getElementById("bt")
+var Than=document.querySelector('.main2')
+var mai=document.querySelector('.main')
+var one=document.querySelector('.one')
+var two=document.querySelector('.two')
+var three=document.querySelector('.three')
+var four=document.querySelector('.four')
+var five=document.querySelector('.five')
+var res=document.querySelector('.ans')
 
 
-one.addEventListener("click", function(){
-    ans.innerHTML = "1";
-})
-
-two.addEventListener("click", function(){
-    ans.innerHTML = "2";
-})
-
-three.addEventListener("click", function(){
-    ans.innerHTML = "3";
-})
-
-four.addEventListener("click", function(){
-    ans.innerHTML = "4";
-})
-
-five.addEventListener("click", function(){
-    ans.innerHTML = "5";
-})
+submitButton.addEventListener("click",function(){
+    Than.style.display="block"
+    mai.style.display="none"
+  })
 
 
-function Submitting(){
-    next.classList.add("popup")
+function get1(){
+    res.innerHTML="1";
+    one.style.backgroundColor="lightblue"
+    two.style.backgroundColor="hsl(218, 10%, 23%)"
+    three.style.backgroundColor="hsl(218, 10%, 23%)"
+    four.style.backgroundColor="hsl(218, 10%, 23%)"
+    five.style.backgroundColor="hsl(218, 10%, 23%)"
+}
+function get2(){
+    res.innerHTML="2"
+    two.style.backgroundColor="lightblue"
+    one.style.backgroundColor="hsl(218, 10%, 23%)"
+    three.style.backgroundColor="hsl(218, 10%, 23%)"
+    four.style.backgroundColor="hsl(218, 10%, 23%)"
+    five.style.backgroundColor="hsl(218, 10%, 23%)"
+}
+function get3(){
+    res.innerHTML="3"
+    three.style.backgroundColor="lightblue"
+    one.style.backgroundColor="hsl(218, 10%, 23%)"
+    two.style.backgroundColor="hsl(218, 10%, 23%)"
+    four.style.backgroundColor="hsl(218, 10%, 23%)"
+    five.style.backgroundColor="hsl(218, 10%, 23%)"
+
+}
+function get4(){
+    res.innerHTML="4"
+    four.style.backgroundColor="lightblue"
+    one.style.backgroundColor="hsl(218, 10%, 23%)"
+    two.style.backgroundColor="hsl(218, 10%, 23%)"
+    three.style.backgroundColor="hsl(218, 10%, 23%)"
+    five.style.backgroundColor="hsl(218, 10%, 23%)"
+}
+function get5(){
+    res.innerHTML="5"
+    five.style.backgroundColor="lightblue"
+    one.style.backgroundColor="hsl(218, 10%, 23%)"
+    two.style.backgroundColor="hsl(218, 10%, 23%)"
+    three.style.backgroundColor="hsl(218, 10%, 23%)"
+    four.style.backgroundColor="hsl(218, 10%, 23%)"
 }
